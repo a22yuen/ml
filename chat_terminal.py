dc1980bb35d3521acc08229ff6d625d07f7407f2
Anson Yuen <a22yuen@uwaterloo.ca>
2024-02-19 13:17:21 -0800

    reformat file

--- chat_terminal.py
+++ chat_terminal.py
@@ -13,20 +13,22 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import MessagesPlaceholder
 import argparse
 
+
 def get_model(model_name: str):
-  print(f"Running with {model_name} ====")
-  if model_name == "ollama":
-    from langchain_community.llms import Ollama
-    from langchain_community.embeddings import OllamaEmbeddings
-    llm = Ollama(model="llama2")
-    embeddings = OllamaEmbeddings()
-    return llm, embeddings
-  if model_name == "openai":
-    from langchain_openai import ChatOpenAI
-    from langchain_openai import OpenAIEmbeddings
-    llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
-    embeddings = OpenAIEmbeddings()
-    return llm, embeddings
+    print(f"Running with {model_name} ====")
+    if model_name == "ollama":
+        from langchain_community.llms import Ollama
+        from langchain_community.embeddings import OllamaEmbeddings
+        llm = Ollama(model="llama2")
+        embeddings = OllamaEmbeddings()
+        return llm, embeddings
+    if model_name == "openai":
+        from langchain_openai import ChatOpenAI
+        from langchain_openai import OpenAIEmbeddings
+        llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+        embeddings = OpenAIEmbeddings()
+        return llm, embeddings
+
 
 def main():
     load_dotenv()
@@ -37,51 +39,51 @@ def main():
 
     model_name = "ollama" if args.local else "openai"
     llm, embeddings = get_model(model_name)
-    
+
     # Load the document, split it into chunks, embed each chunk and load it into the vector store.
-    loader = DirectoryLoader("data/") # not working
+    loader = DirectoryLoader("data/")  # not working
     documents = TextLoader("data/dog.txt").load()
     if not documents:
         print("No documents found in data directory")
         sys.exit()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     docs = text_splitter.split_documents(documents)
-    db = Chroma.from_documents(docs, embeddings) 
+    db = Chroma.from_documents(docs, embeddings)
     retriever = db.as_retriever(search_kwargs={"k": 1})
 
     # First we need a prompt that we can pass into an LLM to generate this search query
     prompt = ChatPromptTemplate.from_messages([
         MessagesPlaceholder(variable_name="chat_history"),
         ("user", "{input}"),
-        ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
+        ("user",
+         "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
     ])
     retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
-    
+
     # Prompt to answer the question
     prompt = ChatPromptTemplate.from_messages([
         ("system", "Answer the user's questions based on the below context:\n\n{context}"),
         MessagesPlaceholder(variable_name="chat_history"),
         ("user", "{input}"),
-    ])  
+    ])
     document_chain = create_stuff_documents_chain(llm, prompt)
 
     retrieval_chain = create_retrieval_chain(retriever_chain, document_chain)
-    
+
     chat_history = []
     print("initial query", args.input)
     query = args.input
     while True:
-      if not query:
-        query = input("Prompt: ")
-      if query in ['quit', 'q', 'exit']:
-        sys.exit()
-      result = retrieval_chain.invoke({"input": query, "chat_history": chat_history})
-      print("result", result['answer'])
+        if not query:
+            query = input("Prompt: ")
+        if query in ['quit', 'q', 'exit']:
+            sys.exit()
+        result = retrieval_chain.invoke({"input": query, "chat_history": chat_history})
+        print("result", result['answer'])
 
-      chat_history.append((query, result['answer']))
-      query = None
+        chat_history.append((query, result['answer']))
+        query = None
+
 
 if __name__ == "__main__":
     main()
-
-
